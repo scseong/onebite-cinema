ca325a8b8af0c05855ee974f8f8211d68474b117
scseong <goidtv@naver.com>
2025-04-01 00:44:21 +0900

    Feat: 검색 기능 구현 및 스타일링 - 검색어 입력 폼을 제출하면 검색 결과 페이지로 이동 - 검색어 초기화 버튼 추가 - 검색어 미입력 시 경고 메시지 표시 및 입력 필드 자동 포커스

--- onebite-cinema-page-router/src/components/searchable-layout.tsx
+++ onebite-cinema-page-router/src/components/searchable-layout.tsx
@@ -1,13 +1,64 @@
-import { PropsWithChildren } from "react";
+import { useRouter } from "next/router";
+import { useState, useEffect, useRef, PropsWithChildren } from "react";
+import CancelIcon from "../../public/cancel.svg";
+import style from "./searchable-layout.module.scss";
 
 export default function SearchableLayout({ children }: PropsWithChildren) {
+  const [search, setSearch] = useState("");
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const router = useRouter();
+  const q = router.query.q as string;
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleClearButtonClick = () => {
+    setSearch("");
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!search) {
+      window.alert("검색어를 입력해주세요.");
+      inputRef.current?.focus();
+      return;
+    }
+
+    router.push(`/search?q=${search}`);
+  };
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
   return (
-    <div>
-      <div>
-        <input type="search" placeholder="검색어를 입력하세요 ..." />
+    <>
+      <form onSubmit={handleSubmit} role="search" className={style.form}>
+        <div className={style.searchbox_container}>
+          <input
+            type="search"
+            name="query"
+            placeholder="검색어를 입력하세요 ..."
+            aria-label="영화 제목으로 검색"
+            value={search}
+            onChange={handleInputChange}
+            ref={inputRef}
+          />
+          {search && (
+            <button
+              type="button"
+              className={style.clear_btn}
+              onClick={handleClearButtonClick}
+            >
+              <CancelIcon role="image" aria-label="검색어 초기화" />
+            </button>
+          )}
+        </div>
         <button>검색</button>
-      </div>
+      </form>
       {children}
-    </div>
+    </>
   );
 }
