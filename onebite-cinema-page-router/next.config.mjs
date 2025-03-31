e3f5afee80b1e84f086871580cc09f7cc058eeb0
scseong <goidtv@naver.com>
2025-04-01 00:15:59 +0900

    Chore: SVG 컴포넌트 지원을 위한 @svgr/webpack 설정 추가 - @svgr/webpack을 설치하고 next.config.mjs 파일을 수정 - webpack 설정을 통해 .svg 파일을 import할 때 @svgr/webpack 로더를 사용하도록 설정 - SVG 파일을 React 컴포넌트로 활용

--- onebite-cinema-page-router/next.config.mjs
+++ onebite-cinema-page-router/next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
   sassOptions: {
     additionalData: `@use "src/styles/_variables.scss" as *;`,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
