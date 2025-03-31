9b553d4578f596fe34ed9c1450bd85b9773d67a9
scseong <goidtv@naver.com>
2025-03-31 18:46:08 +0900

    Chore: Sass 변수 전역 사용 설정 - Next.js 설정 파일에 Sass 옵션 추가 - additionalData 옵션을 사용해 '_variables.scss' 파일의 변수를 모든 Sass   파일에서 전역적으로 사용할 수 있도록 설정

--- onebite-cinema-page-router/next.config.mjs
+++ onebite-cinema-page-router/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    additionalData: `@use "src/styles/_variables.scss" as *;`,
+  },
 };
 
 export default nextConfig;
