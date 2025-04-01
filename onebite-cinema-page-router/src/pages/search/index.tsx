e87c522b4257e0bcca603594cf130e5c83c5fbcd
scseong <goidtv@naver.com>
2025-04-01 15:55:28 +0900

    Feat: 검색 페이지 UI 및 더미 데이터를 활용한 검색 결과 표시 - 검색 결과로 MovieItem 컴포넌트 한 줄에 3개씩 렌더링 - MovieItem 컴포넌트를 클릭하면 영화 상세 페이지 (~/movie/[id])로 이동 - 더미 데이터 사용

--- onebite-cinema-page-router/src/pages/search/index.tsx
+++ onebite-cinema-page-router/src/pages/search/index.tsx
@@ -1,11 +1,26 @@
 import { useRouter } from "next/router";
 import SearchableLayout from "@/components/searchable-layout";
+import MovieItem from "@/components/movie-item";
+import movies from "@/mock/movies-dummy.json";
+import style from "./index.module.scss";
 
 export default function Search() {
   const router = useRouter();
   const { q } = router.query;
 
-  return <h1>검색 결과: {q}</h1>;
+  return (
+    <section className={style.search}>
+      <h2>검색 결과: {q}</h2>
+      <hr />
+      <div>
+        <ul>
+          {movies.map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 Search.getLayout = (page: React.ReactNode) => {
