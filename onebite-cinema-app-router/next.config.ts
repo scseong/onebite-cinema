ae839d58b43ca6399fc4b2433d2c6caea4bff95d
scseong <goidtv@naver.com>
2025-04-10 14:06:08 +0900

    Chore: 디버깅을 위해 로깅 활성화

--- onebite-cinema-app-router/next.config.ts
+++ onebite-cinema-app-router/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
