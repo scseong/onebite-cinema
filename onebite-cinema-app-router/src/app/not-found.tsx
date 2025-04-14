37b7c86bb5962fd46c12733cf68d176ee12bf909
scseong <goidtv@naver.com>
2025-04-15 01:02:56 +0900

    Feat: 정적 페이지로 생성 및 무비 페이지 404 처리 - 검색 페이지(`/search`)를 제외한 모든 페이지를 Static Generation(SSG)으로 구성 - 동적 라우팅 무비 페이지(`/movies/:id`)에 대해:   - `getStaticPaths`에서 빌드 타임에 존재하는 모든 영화 ID 기반 경로 생성 - 404 에러 페이지

---
+++ onebite-cinema-app-router/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <div>
+      <img
+        src="/not-found.png"
+        alt="not-found"
+        style={{ width: "100%", aspectRatio: "16/10" }}
+      />
+    </div>
+  );
+}
